51fa19d2b68deb887710838ff071a3cc130330a9
Alex de Matos Caceres <alexdematoscaceres@gmail.com>
2023-06-06 21:40:25 -0300

    Novas Aulas

---
+++ C#/Colecao/Colecao/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Vetores
+
+            // declarando um Array de x posições:
+            //int[] numeros = new int[5];
+
+            //// atribuindo valores nas posições:
+            //numeros[0] = 0;
+            //numeros[1] = 1;
+            //numeros[2] = 2;
+            //numeros[3] = 3;
+            //numeros[4] = 4;
+
+            //// reatribuindo um valor x na posição y
+            //numeros[2] = 5;
+
+            //string[] nomes = new string[5];
+
+            //nomes[0] = "Alex";
+            //nomes[1] = "Bianca";
+            //nomes[2] = "Mario";
+
+            // Iniciando e Atribuindo valores a um array
+            //       indices:        0          1
+            //string[] filmes = { "Harry Potter", "Homem de Ferro" };
+
+            //Console.WriteLine(numeros[2]);
+            //Console.WriteLine(nomes[2]);
+            //Console.WriteLine(filmes[1]);
+            //Console.Write("Pressione uma Tecla...");
+            //Console.ReadKey();
+
+            #endregion
+
+
+            #region Matrizes
+
+            // Definindo uma matriz:
+            
+            int[,] numeros = new int[3, 3]; // matriz com 3 linhas e 3 colunas
+
+            // para preencher essa matriz:
+
+            // [linha, coluna]
+            numeros[0,0] = 1;
+            numeros[0,1] = 2;
+            numeros[0,2] = 3;
+
+            numeros[1, 0] = 4;
+            numeros[1, 1] = 5;
+            numeros[1, 2] = 6;
+
+            numeros[2, 0] = 7;
+            numeros[2, 1] = 8;
+            numeros[2, 2] = 9;
+
+            string[,] nomes =
+            {
+                // coluna
+              { "Alex", "Gabriel", "Jonas" },  // a primeira linha vai definir as configurações das futuras linhas
+              { "Silvana", "Marina", "Laysa" }, // linhas
+            };
+
+            Console.Write("[" + numeros[0, 0] + ", " + numeros[0, 1] + ", " + numeros[0, 2] + "]\n");
+            Console.Write("[" + numeros[1, 0] + ", " + numeros[1, 1] + ", " + numeros[1, 2] + "]\n");
+            Console.Write("[" + numeros[2, 0] + ", " + numeros[2, 1] + ", " + numeros[2, 2] + "]\n");
+
+            // [1, 2, 3]
+            // [4, 5, 6]
+            // [7, 8, 9]
+
+            Console.WriteLine(nomes[0, 1]);
+
+            Console.Write("Pressione Qualquer Tecla...");
+            Console.ReadKey();
+
+            #endregion
+
+        }
+    }
+}
---
+++ C#/MultiplicarMatrizes/MultiplicarMatrizes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicarMatrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz1 = new int[2,3]; // matriz com duas linhas e 3 colunas
+            int[,] matriz2 = new int[3,2]; // matriz com três linhas e 2 colunas
+            int[,] resultado = new int[2,2]; // matriz com duas linhas e 2 colunas
+
+            Console.WriteLine("Preencha a #1 matriz");
+
+            // percorre todas as linhas
+            for(int linha = 0; linha < 2; linha++)
+            {
+                // percorre todas as colunas
+                for (int coluna = 0; coluna < 3; coluna++)
+                {
+                    Console.Write("#1. Posição [" + linha + "][" + coluna + "]: ");
+                    matriz1[linha, coluna] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nPreencha a #2 matriz");
+
+            // percorre todas as linhas
+            for (int linha = 0; linha < 3; linha++)
+            {
+                // percorre todas as colunas
+                for (int coluna = 0; coluna < 2; coluna++)
+                {
+                    Console.Write("#2. Posição [" + linha + "][" + coluna + "]: ");
+                    matriz2[linha, coluna] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\nO resultado da matriz #1 x matriz2:");
+
+            resultado[0,0] = (matriz1[0,0] * matriz2[0,0]) + (matriz1[0, 1] * matriz2[1, 0]) + (matriz1[0, 2] * matriz2[2, 0]);
+            resultado[1,0] = (matriz1[1,0] * matriz2[0,0]) + (matriz1[1, 1] * matriz2[1, 0]) + (matriz1[1, 2] * matriz2[2, 0]);
+            resultado[0,1] = (matriz1[0,0] * matriz2[0,1]) + (matriz1[0, 1] * matriz2[1, 1]) + (matriz1[0, 2] * matriz2[2, 1]);
+            resultado[1,1] = (matriz1[1,0] * matriz2[0,1]) + (matriz1[1, 1] * matriz2[1, 1]) + (matriz1[1, 2] * matriz2[2, 1]);
+
+            Console.WriteLine("[" + resultado[0,0] + "][" + resultado[0,1]+"]");
+            Console.WriteLine("[" + resultado[1,0] + "][" + resultado[1,1]+"]");
+
+            Console.Write("\nPressione uma tecla para finalizar: ");
+            Console.ReadKey();
+
+        }
+    }
+}
