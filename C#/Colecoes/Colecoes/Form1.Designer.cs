e0bedf0ef8e3c98f8f91f7b3447ce75e3788f107
Alex de Matos Caceres <alexdematoscaceres@gmail.com>
2023-08-19 21:48:25 -0300

    novas aulas

--- C#/Colecoes/Colecoes/Form1.Designer.cs
+++ C#/Colecoes/Colecoes/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ManipularNumeros
+﻿namespace Colecoes
 {
     partial class Form1
     {
@@ -28,50 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnExecutar = new System.Windows.Forms.Button();
-            this.labelResultado = new System.Windows.Forms.Label();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.btnList = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btnExecutar
+            // lista
             // 
-            this.btnExecutar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnExecutar.Location = new System.Drawing.Point(418, 269);
-            this.btnExecutar.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
-            this.btnExecutar.Name = "btnExecutar";
-            this.btnExecutar.Size = new System.Drawing.Size(113, 41);
-            this.btnExecutar.TabIndex = 0;
-            this.btnExecutar.Text = "Executar";
-            this.btnExecutar.UseVisualStyleBackColor = true;
-            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            this.lista.FormattingEnabled = true;
+            this.lista.ItemHeight = 23;
+            this.lista.Location = new System.Drawing.Point(12, 12);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(429, 418);
+            this.lista.TabIndex = 0;
             // 
-            // labelResultado
+            // btnList
             // 
-            this.labelResultado.Location = new System.Drawing.Point(43, 49);
-            this.labelResultado.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.labelResultado.Name = "labelResultado";
-            this.labelResultado.Size = new System.Drawing.Size(489, 34);
-            this.labelResultado.TabIndex = 1;
+            this.btnList.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnList.Location = new System.Drawing.Point(447, 12);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(239, 35);
+            this.btnList.TabIndex = 1;
+            this.btnList.Text = "List";
+            this.btnList.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(575, 343);
-            this.Controls.Add(this.labelResultado);
-            this.Controls.Add(this.btnExecutar);
+            this.ClientSize = new System.Drawing.Size(698, 439);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.lista);
             this.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Listas Genericas";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnExecutar;
-        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Button btnList;
     }
 }
 
--- C#/ManipularNumeros/ManipularNumeros/Form1.cs
+++ C#/ManipularNumeros/ManipularNumeros/Form1.cs
@@ -16,5 +16,65 @@ namespace ManipularNumeros
         {
             InitializeComponent();
         }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            #region TryParse
+
+            // int num;
+            // bool res = int.TryParse("2004", out num); // vai tentar converter a string para numero e vai jogar o valor na variavel num
+            // // caso não consiga converter ele vai atribuir zero na variavel de saida
+
+            // if (res)
+            // {
+            //    labelResultado.Text = num.ToString();
+            // }
+            // else
+            // {
+            //    labelResultado.Text = "Erro na conversão";
+            // } 
+
+            #endregion
+
+            #region Método ToString
+
+            // double num = 120.5;
+
+            // podemos colocar formatação da string colocando parametro dentro do ToString
+            // esse metodo só muda a formatação, não o tipo da variavel
+            // e caso coloque duas casas após a virgula ele só mostrará o que foi colocado, caso tenha mais ele ignorará o restante dos numeros após a virgula
+            // labelResultado.Text = num.ToString("00000.00"); // para mudar as unidades
+            // labelResultado.Text = num.ToString("#.00"); // para mudar apenas as casas decimais
+
+            // int numero = 10;
+
+            // caso a letra estiver em lowerCase ele mostra as letras Hexadecimal nesse formato, porém se estiver em upperCase ele mostrará nesse formato
+            // labelResultado.Text =  numero.ToString("x<digitos que serão mostrados>"); // para mostrar o numero que está na variavel em formato Hexadecimal
+            // labelResultado.Text =  numero.ToString("d<digitos que serão mostrados>"); // para mostrar o numero que está na variavel em formato decimal
+            // labelResultado.Text = numero.ToString("C"); // para mostrar o numero que está na variavel em formato monetario
+            // labelResultado.Text = Convert.ToString(numero, <base (Ex: base decimal, hexadecimal etc)>); // para mostrar o numero que está na variavel em formato x
+            // labelResultado.Text = Convert.ToString(numero, 2); // converter para binario
+
+            #endregion
+
+            #region Classe Math
+
+            // double pi = Math.PI;
+            // double e = Math.E;
+            // double seno = Math.Sin(3);
+            // double conseno = Math.Sin(3);
+            // double potencia = Math.Pow(2,5);
+            // double raizQuadrada = Math.Sqrt(49);
+            // double arredondarMaisProximo = Math.Round(158.75); 
+            // double retornaApenasNumeroInteiro = Math.Truncate(32.57);
+            // double arredondaParaBaixo = Math.Floor(55.6);
+            //double arredondaParaCima = Math.Ceiling(55.4);
+
+            // labelResultado.Text = pi.ToString("#.00");
+            //labelResultado.Text = arredondaParaCima.ToString("#.00");
+
+            #endregion
+
+        }
     }
 }
