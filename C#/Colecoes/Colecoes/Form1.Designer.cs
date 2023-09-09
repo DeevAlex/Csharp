46160a2856601f2ea45b4109aa0ef15be8a4c4b0
Alex de Matos Caceres <alexdematoscaceres@gmail.com>
2023-09-09 18:26:29 -0300

    novas aulas

--- C#/Colecoes/Colecoes/Form1.Designer.cs
+++ C#/Colecoes/Colecoes/Form1.Designer.cs
@@ -30,6 +30,13 @@
         {
             this.lista = new System.Windows.Forms.ListBox();
             this.btnList = new System.Windows.Forms.Button();
+            this.btnHashSet = new System.Windows.Forms.Button();
+            this.btnDictionary = new System.Windows.Forms.Button();
+            this.btnSortedList = new System.Windows.Forms.Button();
+            this.btnSortedDictionary = new System.Windows.Forms.Button();
+            this.btnSortedSet = new System.Windows.Forms.Button();
+            this.btnQueue = new System.Windows.Forms.Button();
+            this.btnStack = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lista
@@ -38,7 +45,7 @@
             this.lista.ItemHeight = 23;
             this.lista.Location = new System.Drawing.Point(12, 12);
             this.lista.Name = "lista";
-            this.lista.Size = new System.Drawing.Size(429, 418);
+            this.lista.Size = new System.Drawing.Size(429, 326);
             this.lista.TabIndex = 0;
             // 
             // btnList
@@ -52,11 +59,95 @@
             this.btnList.UseVisualStyleBackColor = true;
             this.btnList.Click += new System.EventHandler(this.btnList_Click);
             // 
+            // btnHashSet
+            // 
+            this.btnHashSet.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHashSet.Location = new System.Drawing.Point(447, 53);
+            this.btnHashSet.Name = "btnHashSet";
+            this.btnHashSet.Size = new System.Drawing.Size(239, 35);
+            this.btnHashSet.TabIndex = 2;
+            this.btnHashSet.Text = "HashSet";
+            this.btnHashSet.UseVisualStyleBackColor = true;
+            this.btnHashSet.Click += new System.EventHandler(this.btnHashSet_Click);
+            // 
+            // btnDictionary
+            // 
+            this.btnDictionary.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDictionary.Location = new System.Drawing.Point(447, 94);
+            this.btnDictionary.Name = "btnDictionary";
+            this.btnDictionary.Size = new System.Drawing.Size(239, 35);
+            this.btnDictionary.TabIndex = 3;
+            this.btnDictionary.Text = "Dictionary";
+            this.btnDictionary.UseVisualStyleBackColor = true;
+            this.btnDictionary.Click += new System.EventHandler(this.btnDictionary_Click);
+            // 
+            // btnSortedList
+            // 
+            this.btnSortedList.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSortedList.Location = new System.Drawing.Point(447, 135);
+            this.btnSortedList.Name = "btnSortedList";
+            this.btnSortedList.Size = new System.Drawing.Size(239, 35);
+            this.btnSortedList.TabIndex = 4;
+            this.btnSortedList.Text = "SortedList";
+            this.btnSortedList.UseVisualStyleBackColor = true;
+            this.btnSortedList.Click += new System.EventHandler(this.btnSortedList_Click);
+            // 
+            // btnSortedDictionary
+            // 
+            this.btnSortedDictionary.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSortedDictionary.Location = new System.Drawing.Point(447, 176);
+            this.btnSortedDictionary.Name = "btnSortedDictionary";
+            this.btnSortedDictionary.Size = new System.Drawing.Size(239, 35);
+            this.btnSortedDictionary.TabIndex = 5;
+            this.btnSortedDictionary.Text = "SortedDictionary";
+            this.btnSortedDictionary.UseVisualStyleBackColor = true;
+            this.btnSortedDictionary.Click += new System.EventHandler(this.btnSortedDictionary_Click);
+            // 
+            // btnSortedSet
+            // 
+            this.btnSortedSet.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSortedSet.Location = new System.Drawing.Point(447, 217);
+            this.btnSortedSet.Name = "btnSortedSet";
+            this.btnSortedSet.Size = new System.Drawing.Size(239, 35);
+            this.btnSortedSet.TabIndex = 6;
+            this.btnSortedSet.Text = "SortedSet";
+            this.btnSortedSet.UseVisualStyleBackColor = true;
+            this.btnSortedSet.Click += new System.EventHandler(this.btnSortedSet_Click);
+            // 
+            // btnQueue
+            // 
+            this.btnQueue.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnQueue.Location = new System.Drawing.Point(447, 258);
+            this.btnQueue.Name = "btnQueue";
+            this.btnQueue.Size = new System.Drawing.Size(239, 35);
+            this.btnQueue.TabIndex = 7;
+            this.btnQueue.Text = "Queue";
+            this.btnQueue.UseVisualStyleBackColor = true;
+            this.btnQueue.Click += new System.EventHandler(this.btnQueue_Click);
+            // 
+            // btnStack
+            // 
+            this.btnStack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStack.Location = new System.Drawing.Point(447, 299);
+            this.btnStack.Name = "btnStack";
+            this.btnStack.Size = new System.Drawing.Size(239, 35);
+            this.btnStack.TabIndex = 8;
+            this.btnStack.Text = "Stack";
+            this.btnStack.UseVisualStyleBackColor = true;
+            this.btnStack.Click += new System.EventHandler(this.btnStack_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(698, 439);
+            this.ClientSize = new System.Drawing.Size(698, 339);
+            this.Controls.Add(this.btnStack);
+            this.Controls.Add(this.btnQueue);
+            this.Controls.Add(this.btnSortedSet);
+            this.Controls.Add(this.btnSortedDictionary);
+            this.Controls.Add(this.btnSortedList);
+            this.Controls.Add(this.btnDictionary);
+            this.Controls.Add(this.btnHashSet);
             this.Controls.Add(this.btnList);
             this.Controls.Add(this.lista);
             this.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -72,6 +163,13 @@
 
         private System.Windows.Forms.ListBox lista;
         private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Button btnHashSet;
+        private System.Windows.Forms.Button btnDictionary;
+        private System.Windows.Forms.Button btnSortedList;
+        private System.Windows.Forms.Button btnSortedDictionary;
+        private System.Windows.Forms.Button btnSortedSet;
+        private System.Windows.Forms.Button btnQueue;
+        private System.Windows.Forms.Button btnStack;
     }
 }
 
--- C#/Colecoes/Colecoes/Form1.cs
+++ C#/Colecoes/Colecoes/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,6 +21,11 @@ namespace Colecoes
         private void btnList_Click(object sender, EventArgs e)
         {
 
+            // limpar a lista antes de gerar o codigo abaixo
+            lista.Items.Clear();
+
+            #region List
+
             // antes
 
             string[] nomes = new string[3];
@@ -28,25 +34,50 @@ namespace Colecoes
             nomes[1] = "Juliana";
             nomes[2] = "Juliano";
 
-            #region List
-
             // lista generica
             List<string> nomes2 = new List<string>();
 
             // adicionar
-            nomes2.Add("Alex"); // 0 // o parametro aqui deve ser o mesmo do que está entre os <> da List ali em cima
-            nomes2.Add("Marina"); // 1
+            nomes2.Add("Marina"); // 0
+            nomes2.Add("Alex"); // 1 // o parametro aqui deve ser o mesmo do que está entre os <> da List ali em cima
             nomes2.Add("Johnson"); // 2
             nomes2.Add("Mike"); // 3
 
             // adicionar o valor da posição do array que a gente passa como parametro
             nomes2.AddRange(nomes);
 
+            // retorna um valor booleano se contem o valor x na lista
+            // if (nomes2.Contains("Johnson"))
+            // {
+            //    MessageBox.Show("Contém");
+            // } else
+            // {
+            //    MessageBox.Show("Não contém");
+            // }
+
+            // retorna quantos valores tem dentro da lista
+            // MessageBox.Show("Numero de Elementos: " + nomes2.Count());
+
+            // Ordena a Lista em ordem alfabetica
+            // nomes2.Sort();
+
+            // retorna o indice do valor na lista, quando retorna -1 é porque não está na lista
+            //MessageBox.Show("Johnson está no indice: " + nomes2.IndexOf("Johnson"));
+
             // remover
-            nomes2.Remove("Mike");
+            //nomes2.Remove("Mike");
 
             // para mostrar na listbox
 
+            // insere um valor na posição x
+            // nomes2.Insert(2, "Vitoria");
+
+            // remover um valor na posição x
+            // nomes2.RemoveAt(2);
+
+            // limpa toda a lista
+            //nomes2.Clear();
+            
             foreach (string nome in nomes2)
             {
                 lista.Items.Add(nome);
@@ -54,6 +85,258 @@ namespace Colecoes
 
             #endregion
 
+
+        }
+
+        private void btnHashSet_Click(object sender, EventArgs e)
+        {
+
+            lista.Items.Clear();
+
+            #region HashSet
+
+            // a lista do tipo HashSet é semelhante a uma lista do tipo List porém ela não permite items repetidos
+
+            HashSet<string> veiculos = new HashSet<string>()
+            {
+                // podemos adicionar items na lista na inicialização assim:
+                "Carro", "Moto",
+                "Aviao", "Helicoptero", "Barco"
+            };
+
+            // na lista HashSet não retorna o valor que esta no indice assim veiculos[1]
+
+            // para pegar o valor na posição x na lista HashSet tem que ser assim:
+            //MessageBox.Show(veiculos.ElementAt(2));
+
+            // adicionando - no caso a HashSet não tem o addRange
+            // veiculos.Add("Citroen C4");
+
+            // if (veiculos.Add("Moto"))
+            // {
+            //    MessageBox.Show("Item adicionado");
+            // } else
+            // {
+            //    MessageBox.Show("Item não adicionado");
+            // }
+
+            // if (veiculos.Contains("Barco"))
+            // {
+            //    MessageBox.Show("Contém");
+            // } else
+            // {
+            //    MessageBox.Show("Não Contém");
+            // }
+
+            //veiculos.Remove("Barco");
+
+            //veiculos.Count();
+
+            //veiculos.Clear();
+
+            // também tem na lista do tipo List:
+
+            // retorna o valor do(a) primeiro/cabeça da lista HashSet
+            //veiculos.First();
+
+            // retorna o valor da ultima posição/calda da lista HashSet
+            //veiculos.Last();
+
+            foreach (string veiculo in veiculos)
+            {
+                lista.Items.Add(veiculo);
+            }
+
+            #endregion
+
+        }
+
+        private void btnDictionary_Click(object sender, EventArgs e)
+        {
+
+            #region Dictionary
+
+            lista.Items.Clear();
+
+            // Dictionary<tipo da chave, tipo do elemento>
+            Dictionary<int, string> alunos = new Dictionary<int, string>()
+            {
+                { 0, "Gabriel" },
+                { 50, "Joana" },
+                { 150, "Pietra" },
+            };
+
+            // não podemos adicionar a mesma chave se ela ja existe, porém podemos ter os mesmos valores
+            alunos.Add(100, "Alex");
+            alunos.Remove(0);
+
+            // percorrer um dictionary usando um foreach:
+            foreach(KeyValuePair<int, string> valores in alunos)
+            {
+                // mostra chave e valor na lista:
+                //lista.Items.Add(valores); - Output: [chave, valor]
+                lista.Items.Add(valores.Key + " - " + valores.Value); // acessando apenas a chave e o valor sem os []
+            }
+
+            // mostrar quantos elementos tem na lista
+            //alunos.Count();
+
+            // para limpar a lista
+            //alunos.Clear();
+
+            // verifica se contém a chave e retorna um boolean se existe ou não na lista
+            //alunos.ContainsKey(0);
+
+            // verifica se contém o valor e retorna um boolean se existe ou não na lista
+            //alunos.ContainsValue("Joana");
+
+            // Retorna o primeiro/cabeça par (exemplo de par: [chave, valor]) da lista, para acessar chave/valor alunos.First().key/value
+            //alunos.First();
+
+            // Retorna o(a) ultimo/calda par (exemplo de par: [chave, valor]) da lista, para acessar chave/valor alunos.First().key/value
+            //alunos.Last();
+
+            #endregion
+
+
+        }
+
+        private void btnSortedList_Click(object sender, EventArgs e)
+        {
+
+            #region SortedList
+
+            lista.Items.Clear();
+
+            // lista ordenada - usa menos memoria
+            SortedList<int, string> alunos = new SortedList<int, string>()
+            {
+                { 27, "Danny" },
+                { 10, "Gabriel" },
+                { 17, "Arthur" },
+            };
+
+            alunos.Add(14, "Roberto");
+
+            // remover pelo indice
+            // alunos.remove(27);
+            // alunos.RemoveAt(10);
+
+            // alunos.Count();
+
+            // retorna um boolean se contém ou não a chave x
+            // alunos.ContainsKey(57);
+
+            // retorna um boolean se contém ou não o valor x
+            // retorna um boolean se con117. SortedDictionarytém ou não o valor x
+            // retorna um boolean se con117. SortedDictionarytém ou não o valor x
+            // retorna um boolean se con117. SortedDictionarytém ou não o valor x
+            // retorna um boolean se con117. SortedDictionarytém ou não o valor x
+            // retorna um boolean se con117. SortedDictionarytém ou não o valor x
+            // retorna um boolean se con117. SortedDictionarytém ou não o valor x
+            // retorna um boolean se con117. SortedDictionarytém ou não o valor x
+            // retorna um boolean se con117. SortedDictionarytém ou não o valor x
+            // alunos.ContainsValue("Alex");
+
+            // alunos.Reverse(), inverte a ordem em que a lista estava
+            foreach (KeyValuePair<int, string> item in alunos)
+            {
+                lista.Items.Add(item);
+            }
+
+            #endregion
+
+        }
+
+        private void btnSortedDictionary_Click(object sender, EventArgs e)
+        {
+
+            #region SortedDictionary
+
+            lista.Items.Clear();
+
+            // usa mais memoria em relação a SortedList, porém tem operações mais rapidas em inserções e remoções de dados por exemplo, é mais diferenças tecnicas 
+            SortedDictionary<int, string> alunos = new SortedDictionary<int, string>()
+            {
+                // ordena na ordem crescente e na ordem alfabetica
+                { 4512, "Gabriel" },
+                { 1542, "Arthur" },
+                { 2145, "Danny" },
+            };
+
+            alunos.Add(5478, "Flavio");
+
+            // alunos.Remove(120);
+
+            // alunos.Count();
+
+            // alunos.Clear();
+
+            // alunos.ContainsKey(15);
+
+            // alunos.ContainsValue("Alex");
+
+            // aqui você coloca o indice, o indice é diferente da chave que foi colocada na adição a lista
+            MessageBox.Show("Elemento #0: " + alunos.ElementAt(0));
+
+            // aqui podemos colocar o alunos.Reverse(); também
+            foreach (KeyValuePair<int, string> item in alunos)
+            {
+                lista.Items.Add(item);
+            }
+
+            #endregion
+
+        }
+
+        private void btnSortedSet_Click(object sender, EventArgs e)
+        {
+
+            #region SortedSet
+
+            lista.Items.Clear();
+
+            SortedSet<string> alunos = new SortedSet<string>()
+            {
+                "Alex",
+                "Jonas",
+                "Silveira",
+                "Simone",
+                "Marina",
+            };
+
+            foreach (string item in alunos)
+            {
+                lista.Items.Add(item);
+            }
+
+            #endregion
+
+        }
+
+        private void btnQueue_Click(object sender, EventArgs e)
+        {
+
+            #region Queue
+
+            lista.Items.Clear();
+
+            Queue<String> fila = new Queue<string>();
+
+            #endregion
+
+        }
+
+        private void btnStack_Click(object sender, EventArgs e)
+        {
+
+            #region Stack
+
+            lista.Items.Clear();
+
+            Stack<int> pilha = new Stack<int>();
+            
+            #endregion
 
         }
     }
