9117e97486b6a5d7bf8f7f5295539989b61529fe
Alex de Matos Caceres <alexdematoscaceres@gmail.com>
2023-10-02 21:18:16 -0300

    novas aulas

--- C#/ClassesImportantes/ClassesImportantes/Form1.Designer.cs
+++ C#/ClassesImportantes/ClassesImportantes/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnMessageBox = new System.Windows.Forms.Button();
+            this.lbResultado = new System.Windows.Forms.Label();
+            this.btnAleatorio = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMessageBox
+            // 
+            this.btnMessageBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMessageBox.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMessageBox.Location = new System.Drawing.Point(465, 334);
+            this.btnMessageBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnMessageBox.Name = "btnMessageBox";
+            this.btnMessageBox.Size = new System.Drawing.Size(188, 64);
+            this.btnMessageBox.TabIndex = 0;
+            this.btnMessageBox.Text = "MessageBox";
+            this.btnMessageBox.UseVisualStyleBackColor = true;
+            this.btnMessageBox.Click += new System.EventHandler(this.btnMessageBox_Click);
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResultado.Location = new System.Drawing.Point(24, 33);
+            this.lbResultado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(602, 59);
+            this.lbResultado.TabIndex = 1;
+            this.lbResultado.Text = "label1";
+            // 
+            // btnAleatorio
+            // 
+            this.btnAleatorio.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAleatorio.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAleatorio.Location = new System.Drawing.Point(465, 251);
+            this.btnAleatorio.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAleatorio.Name = "btnAleatorio";
+            this.btnAleatorio.Size = new System.Drawing.Size(188, 64);
+            this.btnAleatorio.TabIndex = 2;
+            this.btnAleatorio.Text = "Random";
+            this.btnAleatorio.UseVisualStyleBackColor = true;
+            this.btnAleatorio.Click += new System.EventHandler(this.btnAleatorio_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(666, 412);
+            this.Controls.Add(this.btnAleatorio);
+            this.Controls.Add(this.lbResultado);
+            this.Controls.Add(this.btnMessageBox);
+            this.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnMessageBox;
+        private System.Windows.Forms.Label lbResultado;
+        private System.Windows.Forms.Button btnAleatorio;
     }
 }
 
--- C#/ClassesImportantes/ClassesImportantes/Form1.cs
+++ C#/ClassesImportantes/ClassesImportantes/Form1.cs
@@ -16,5 +16,38 @@ namespace ClassesImportantes
         {
             InitializeComponent();
         }
+
+        private void btnMessageBox_Click(object sender, EventArgs e)
+        {
+
+            // Messagebox normal - MessageBox.Show("Ola");
+            // Messagebox com titulo - MessageBox.Show("Mensagem a ser impressa", "Titulo da Mensagem");
+            // Messagebox definindo os botões - MessageBox.Show("Texto da Mensagem", "Titulo", MessageBoxButtons.YesNo);
+            // pegando o valor do botão clicado - DialogResult resultado = MessageBox.Show("Texto da Mensagem", "Titulo", MessageBoxButtons.YesNoCancel);
+            // DialogResult resultado = MessageBox.Show("Texto da Mensagem", "Titulo", MessageBoxButtons.YesNoCancel);
+
+            // if (resultado == DialogResult.Yes)
+            // {
+            //    lbResultado.Text = "Clicou em Sim";
+            // } else if (resultado == DialogResult.Cancel)
+            // {
+            //    lbResultado.Text = "Clicou em Cancelar";
+            // } else if (resultado != DialogResult.None) {
+            //    lbResultado.Text = "Clicou em Não";
+            // }
+
+            // colocando um icone na mensagem - MessageBox.Show("Mensagem", "Titulo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            // define qual botão vai ficar marcado - MessageBox.Show("Mensagem", "Titulo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+
+        }
+
+        private void btnAleatorio_Click(object sender, EventArgs e)
+        {
+
+            Random r = new Random(1); // o parametro aqui é o numero que vai ser utilizado para calcular o inicio do valor da sequencia numerica, utilizado para manipular os tipos de numeros aleatorios que serão gerados
+
+            lbResultado.Text = r.Next(0, 100).ToString(); // no parametro definimos até qual numero ele vai gerar, minimo - numero definido
+
+        }
     }
 }
